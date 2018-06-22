a08b1ca39c50beb8580595d8cc397a7fb39fd105
bachtiar168 <40377143+bachtiar168@users.noreply.github.com>
2018-06-22 18:43:42 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -19,7 +19,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*11)+ 1);
+var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2018/06/best-mt4-advantage-and-earning-through.html"}
 if(x=="2"){xxx="/2018/06/forex-demo-account-forex-basics.html"}
@@ -27,8 +27,7 @@ if(x=="3"){xxx="/2018/06/the-mt4-expert-advisor-taking-fx-world.html"}
 if(x=="4"){xxx="/2018/06/locating-forex-broker-online-find-best.html"}
 if(x=="5"){xxx="/2018/06/recommended-forex-brokers-3-reasons-to.html"}
 if(x=="6"){xxx="/2018/06/new-automated-forex-software-key-buying.html"}
-if(x=="7"){xxx="/2018/06/forex-money-manager-have-you-considered.html"}
-a_to_ve[i].href="http://www.infobrokerforex.review"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="7"){xxx="/2018/06/forex-money-manager-have-you-considered.html"}a_to_ve[i].href="http://www.infobrokerforex.review"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
